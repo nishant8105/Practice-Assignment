404ddfbe285c51fd0e452f7cb27970121a398528
nishant8105 <nishantjadhav8105@gmail.com>
2026-02-08 12:44:11 +0530

    task 1 complete

---
+++ Assignment_2/Task1.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+if num & 1 == 0 or num == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
+
+"""
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
+"""
