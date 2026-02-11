d1f454d1fcee245e0befb60b2c8a036bb5e646ce
nishant8105 <nishantjadhav8105@gmail.com>
2026-02-11 11:50:39 +0530

    Complete task 1

---
+++ Assignment_3/Task1.py
@@ -0,0 +1,8 @@
+def factorial(num):
+    fact = 1
+    for i in range (1, num+1):
+        fact *= i
+    return fact
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is : {factorial(5)}")
