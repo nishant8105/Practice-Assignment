c0639ebc273a766e1562150ed4e009127ae9dfe1
nishant8105 <nishantjadhav8105@gmail.com>
2026-02-11 11:51:06 +0530

    Complete task 2

---
+++ Assignment_3/Task2.py
@@ -0,0 +1,10 @@
+import math
+number = int (input("Enter the number: "))
+
+square_root = math.sqrt(number)
+Log = math.log(number)
+sin = math.sin(number)
+
+print(f"Square root : {square_root}")
+print(f"Logarithm : {Log}")
+print(f"Sine : {sin}")
