b6220843aea1d04c64350db65f40fc5cf0cbb9fc
nishant8105 <nishantjadhav8105@gmail.com>
2026-02-15 21:20:31 +0530

    use modulas operater

--- Assignment_2/Task1.py
+++ Assignment_2/Task1.py
@@ -1,12 +1,11 @@
 num = int(input("Enter a number: "))
-if num & 1 == 0 or num == 0:
-    print(num, "is even")
-else:
-    print(num, "is odd")
+# if num & 1 == 0 or num == 0:
+#     print(num, "is even")
+# else:
+#     print(num, "is odd")
 
-"""
+
 if num % 2 == 0:
     print(num, "is even")
 else:
     print(num, "is odd")
-"""
