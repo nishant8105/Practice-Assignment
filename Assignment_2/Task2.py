1c008fcce477215f5ad87fdfea7a46efffd7f0ed
nishant8105 <nishantjadhav8105@gmail.com>
2026-02-15 21:18:57 +0530

    Modified Range

--- Assignment_2/Task2.py
+++ Assignment_2/Task2.py
@@ -1,4 +1,4 @@
 sum = 0
-for i in range(1, 50):
-    sum += i
-print(sum)
+for i in range(1, 51): # count from 1 to 50 as 51 is excluded
+    sum += i # for each iteration it add number and save in sum
+print(f"The sum of number from 1 to 50 is : {sum}")
