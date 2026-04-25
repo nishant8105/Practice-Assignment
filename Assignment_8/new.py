4344a915193ce2f8e65956ad9231ff0e8c925523
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-25 08:47:47 +0530

    add first page

--- Assignment_8/new.py
+++ Assignment_8/new.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+import os
 
+picfolder = os.path.join('static')
 # Interaction
 web = Flask(__name__)
 
@@ -16,13 +18,19 @@ def homepage():
     return render_template('register.html')
 
 @web.route("/confirmation", methods=["POST", "GET"])
-def register():
+def confirmation():
     if request.method == "POST":
         n = request.form.get('name')
         c = request.form.get('city')
         p = request.form.get('phone number')
         return render_template('confirmation.html', name = n, city = c, phonenumber = p)
     
+web.config['UPLOAD_FOLDER'] = picfolder
+@web.route("/first")
+def first():
+    pic = os.path.join(web.config['UPLOAD_FOLDER'], 'waterfall.jpg')
+    return render_template('home.html', user_image=pic)
+
 
 if __name__ == "__main__":
     web.run(debug=True)
