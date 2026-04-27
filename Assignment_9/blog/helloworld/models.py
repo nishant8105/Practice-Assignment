1c6589a44da8fb595774afc965d456b780021d63
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-27 08:46:29 +0530

    post models

---
+++ Assignment_9/blog/helloworld/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    ipdated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
