30c913ba25cbe616f23e94416422c2717e4972aa
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-28 17:29:15 +0530

    add created by and updated on for post

--- Assignment_9/blog/helloworld/models.py
+++ Assignment_9/blog/helloworld/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    ipdated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    
     def __str__(self) -> str:
         return self.title
