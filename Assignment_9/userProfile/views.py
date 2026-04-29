f4fd376fae31c6e76617a9a999b3bc2d732f7268
nishant8105 <nishantjadhav8105@gmail.com>
2026-04-29 15:31:17 +0530

    feat: refactor Post API views, models, and serializers

--- Assignment_9/userProfile/views.py
+++ Assignment_9/userProfile/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from userProfile.serializers import PostSerializers
+from rest_framework import generics
+from userProfile.models import Post
+from rest_framework.permissions import IsAuthenticated
+from userProfile.permission import IsPostPossessor
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({
+            'message' : 'Hello World!'
+        })
+
+class PostListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['created_by__username', 'created_on']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_on', 'title']
+
+class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated, IsPostPossessor]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
