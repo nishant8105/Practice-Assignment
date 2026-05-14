ec246cc33e5b3ed2e5572ec6d598a70652bc0530
nishant8105 <nishantjadhav8105@gmail.com>
2026-05-14 14:52:02 +0530

    bilateral Filter

---
+++ Assignment_11/cv_11.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread("./parrot.png")
+
+resize = cv2.resize(img, (520, 520))
+
+d = 7
+
+sigmacolor = 100
+sigmaspace = 100
+
+b = cv2.bilateralFilter(resize, d, sigmacolor, sigmaspace)
+
+cv2.imshow("Input", resize)
+cv2.imshow("Output", b)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
