fa64ac2dd5ee6705fdc987e8f7f93e2d26152c7e
nishant8105 <nishantjadhav8105@gmail.com>
2026-05-14 14:52:43 +0530

    canny

---
+++ Assignment_11/cv_12.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread("./parrot.png", 0)
+
+resize = cv2.resize(img, (520,520))
+min_thresh = 100
+max_thresh = 200
+edges = cv2.Canny(resize, min_thresh, max_thresh)
+
+cv2.imshow("Original", resize)
+cv2.imshow("Edges", edges)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
