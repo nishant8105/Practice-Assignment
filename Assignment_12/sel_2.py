645e91c6eeb201a825329ad5bb6276370514ed26
nishant8105 <nishantjadhav8105@gmail.com>
2026-05-15 17:03:26 +0530

    fine element by name

---
+++ Assignment_12/sel_2.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+driver.maximize_window()
+
+search_box = driver.find_element(By.NAME, 'q')
+search_box.send_keys('selenium')
+time.sleep(5)
+button = driver.find_element(By.NAME, 'btnK')
+button.click()
+
+time.sleep(5)
+driver.back()
+time.sleep(5)
+driver.forward()
+time.sleep(5)
+
+driver.close()
