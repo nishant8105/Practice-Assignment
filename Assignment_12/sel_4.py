ac8225ce2840d7e14286cb28f5e0339cd7f64310
nishant8105 <nishantjadhav8105@gmail.com>
2026-05-16 11:21:00 +0530

    refresh page

---
+++ Assignment_12/sel_4.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+driver = webdriver.Chrome()
+driver.get("https://www.google.com")
+driver.maximize_window()
+
+time.sleep(5)
+
+driver.refresh()
+
+time.sleep(5)
